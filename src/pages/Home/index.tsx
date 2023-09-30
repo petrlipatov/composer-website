c7c76ff2a7fe92d998d1f86b737ddae658055fd4
Petr Lipatov <lipatov3@gmail.com>
2023-10-01 00:16:04 +0300

    styles: Home comp layout and styles added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,27 @@
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
-useRef;
+import imageSrc from "../../assets/images/IMG_8450.png";
 
 export default function Home() {
-  const windowWidth = useRef(window.innerWidth);
-  const windowHeight = useRef(window.innerHeight);
+  const [pageHeight, setPageHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPageHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <div className={styles.page}>
-      <div className={styles.caption}>{`Height: ${windowHeight.current}`}</div>
-      <div className={styles.caption}>{`Width: ${windowWidth.current}`}</div>
+    <div className={styles.page} style={{ height: `${pageHeight}px` }}>
+      <div className={styles.titleContainer}>
+        <div className={styles.name}>LIZA TIKHONOVA</div>
+        <div className={styles.title}>COMPOSER</div>
+      </div>
+      <img className={styles.image} src={imageSrc} alt="" />
     </div>
   );
 }
