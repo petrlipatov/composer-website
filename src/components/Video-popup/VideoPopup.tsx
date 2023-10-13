f0f666ba22b59153dbeb1873e42da64970e9216d
Petr Lipatov <lipatov3@gmail.com>
2023-10-13 18:43:22 +0300

    feat: video-popup comp added, play showreel implemented

--- src/components/Video-popup/VideoPopup.tsx
+++ src/components/Video-popup/VideoPopup.tsx
@@ -1,12 +1,44 @@
 import { createPortal } from "react-dom";
+import styles from "./VideoPopup.module.css";
+import closeIconSrc from "./../../assets/images/close-icon.svg";
+import { Dispatch, SetStateAction } from "react";
 
-function VideoPopup() {
+type VideoPopupProps = {
+  closeFunc: Dispatch<SetStateAction<boolean>>;
+};
+
+function VideoPopup({ closeFunc }: VideoPopupProps) {
   const rootContainer = document.getElementById("modal");
 
+  const handlePopupTouch = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    closeFunc(false);
+  };
+
   return createPortal(
-    <>
-      <h1>Player</h1>
-    </>,
+    <div
+      className={styles.container}
+      onTouchStart={handlePopupTouch}
+      onTouchMove={handlePopupTouch}
+      onTouchEnd={handlePopupTouch}
+    >
+      <button
+        className={styles.closeIcon}
+        style={{ backgroundImage: `url(${closeIconSrc})` }}
+        onClick={() => {
+          closeFunc(false);
+        }}
+      />
+      <iframe
+        id="player"
+        title="All In One Composer Showreel - Liza Tikhonova / Film Music, Experimental, Electronic, Neoclassical"
+        height={280}
+        src="https://www.youtube.com/embed/u0dBG0AL3Cs?enablejsapi=1&origin=https://example.com"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen={true}
+        style={{ border: "none" }}
+      ></iframe>
+    </div>,
     rootContainer
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,19 @@
 import styles from "./index.module.css";
-import { useRef, RefObject } from "react";
-
+import { useRef, RefObject, useState } from "react";
 import imageSrc from "../../assets/images/logo.png";
 import nameSrc from "../../assets/images/name.svg";
 import arrowSrc from "../../assets/images/play-icon.svg";
 import titleSrc from "../../assets/images/title.svg";
 import playSrc from "../../assets/images/play-icon.svg";
 import showreelSrc from "../../assets/images/showreel.svg";
-import cn from "classnames";
 import { gsap } from "gsap";
 import { Observer } from "gsap/Observer";
 import { useLayoutEffect } from "react";
+import VideoPopup from "../../components/Video-popup/VideoPopup";
 
 export default function Home() {
+  const [isPopupOpen, setPopupState] = useState(false);
+
   const tlMain = gsap.timeline();
   const tlArrows = gsap.timeline({ repeat: -1 });
   gsap.registerPlugin(Observer);
@@ -20,13 +21,11 @@ export default function Home() {
   const nameRef: RefObject<HTMLImageElement> = useRef(null),
     titleRef: RefObject<HTMLImageElement> = useRef(null),
     logoRef: RefObject<HTMLImageElement> = useRef(null),
-    playRef: RefObject<HTMLImageElement> = useRef(null),
-    showreelRef: RefObject<HTMLImageElement> = useRef(null),
     showreelContainerRef: RefObject<HTMLImageElement> = useRef(null),
     arrowBottomRef: RefObject<HTMLImageElement> = useRef(null),
     arrowTopRef: RefObject<HTMLImageElement> = useRef(null);
 
-  useLayoutEffect(() => {
+  useLayoutEffect(function startArrowsAnimation() {
     gsap.set(arrowTopRef.current, {
       xPercent: -50,
       rotate: 90,
@@ -82,7 +81,7 @@ export default function Home() {
     );
   }, []);
 
-  useLayoutEffect(function mainAnimation() {
+  useLayoutEffect(function startMainAnimation() {
     gsap.set([nameRef.current, titleRef.current], { xPercent: -50 });
     gsap.set(showreelContainerRef.current, {
       xPercent: -50,
@@ -147,49 +146,41 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
-      <div className={cn(styles.section, styles.section1)}></div>
-      <div className={cn(styles.section, styles.section2)}></div>
-      <div className={cn(styles.section, styles.section3)}></div>
+      <div className={styles.section}></div>
+      <div className={styles.section}></div>
+      <div className={styles.section}></div>
 
       <img className={styles.name} src={nameSrc} alt="name" ref={nameRef} />
       <img className={styles.title} src={titleSrc} alt="title" ref={titleRef} />
-      <div className={styles.playContainer} ref={showreelContainerRef}>
+
+      <div
+        className={styles.playContainer}
+        onClick={() => {
+          setPopupState(true);
+        }}
+        ref={showreelContainerRef}
+      >
         <img
           className={styles.showreel}
           src={showreelSrc}
           alt="showreel-icon"
-          ref={showreelRef}
         />
-        <img
-          className={styles.play}
-          src={playSrc}
-          alt="play-icon"
-          ref={playRef}
-        />
+        <img className={styles.play} src={playSrc} alt="play-icon" />
       </div>
 
       <img
         src={arrowSrc}
         className={styles.arrowTop}
         ref={arrowTopRef}
-        alt=""
+        alt="arrow-icon"
       />
 
       <img
         src={arrowSrc}
         className={styles.arrowBottom}
         ref={arrowBottomRef}
-        alt=""
+        alt="arrow-icon"
       />
-      {/* <iframe
-        width="100%"
-        height="auto"
-        src="https://www.youtube.com/embed/vlDzYIIOYmM"
-        title="GoPro HERO5 + Karma: The Launch in 4K"
-        // frameorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe> */}
 
       <img
         className={styles.image}
@@ -198,6 +189,8 @@ export default function Home() {
         ref={logoRef}
         decoding="async"
       />
+
+      {isPopupOpen && <VideoPopup closeFunc={setPopupState} />}
     </div>
   );
 }
