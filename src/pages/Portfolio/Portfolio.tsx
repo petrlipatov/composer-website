d44db2d5d36972bf7696e3f20b17606d4e84e192
Petr Lipatov <lipatov3@gmail.com>
2023-10-22 14:31:17 +0300

    audio tag test

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,5 +1,17 @@
+import styles from "./Portfolio.module.css";
+
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <audio controls>
+        <source
+          src="https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav"
+          type="audio/mpeg"
+        />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
 }
 
 export default Portfolio;
