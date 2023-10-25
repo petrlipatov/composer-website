2c430cd287a2aeff7ec0d582e28695ed94a999f4
Petr Lipatov <lipatov3@gmail.com>
2023-10-25 22:13:50 +0300

    refactor: Portfolio structure updated

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,23 +1,22 @@
 import styles from "./Portfolio.module.css";
+import { Link } from "react-router-dom";
 import nameSrc from "../../assets/images/name.svg";
 import titleSrc from "../../assets/images/title.svg";
+import Toggler from "./Toggler/Toggler";
 
 function Portfolio() {
   return (
-    <div className={styles.container}>
-      <img
-        className={styles.name}
-        src={nameSrc}
-        alt="name"
-        data-animate="name"
-      />
-      <img
-        className={styles.title}
-        src={titleSrc}
-        alt="title"
-        data-animate="title"
-      />
-      <h1>Portfolio</h1>
+    <div className={styles.page}>
+      <div className={styles.headerSection}>
+        <Link to="/" className={styles.nameContainer}>
+          <img className={styles.name} src={nameSrc} alt="name" />
+          <img className={styles.title} src={titleSrc} alt="title" />
+        </Link>
+      </div>
+
+      <div className={styles.togglerSection}>
+        <Toggler />
+      </div>
     </div>
   );
 }
