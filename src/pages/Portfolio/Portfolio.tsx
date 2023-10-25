a34febfcddbc42d5f03cf704ad3b069bc6ebb20c
Petr Lipatov <lipatov3@gmail.com>
2023-10-25 17:18:11 +0300

    design: added logo to Portfolio comp

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,8 +1,22 @@
 import styles from "./Portfolio.module.css";
+import nameSrc from "../../assets/images/name.svg";
+import titleSrc from "../../assets/images/title.svg";
 
 function Portfolio() {
   return (
     <div className={styles.container}>
+      <img
+        className={styles.name}
+        src={nameSrc}
+        alt="name"
+        data-animate="name"
+      />
+      <img
+        className={styles.title}
+        src={titleSrc}
+        alt="title"
+        data-animate="title"
+      />
       <h1>Portfolio</h1>
     </div>
   );
