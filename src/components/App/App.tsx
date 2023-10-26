0458fa95ad742e42e69aec402fe3ed9d4c70eb73
Petr Lipatov <lipatov3@gmail.com>
2023-10-26 22:33:21 +0300

    feat: Suspense added to routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Home from "../../pages/Home";
 import Layout from "../Layout/Layout";
-import Portfolio from "../../pages/Portfolio/Portfolio";
+
+const Portfolio = lazy(() => import("../../pages/Portfolio/Portfolio"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route
+          path="/portfolio"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Portfolio />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
--- src/components/Audio-player/AudioPlayer.tsx
+++ src/components/Audio-player/AudioPlayer.tsx
@@ -2,39 +2,40 @@
 // import styles from "./Portfolio.module.css";
 // import mp3 from "../../assets/Theory-of-Light-Master.mp3";
 
-// function Portfolio() {
-//   const audioRef = useRef(null);
+function Portfolio2() {
+  //   const audioRef = useRef(null);
 
-//   useEffect(() => {
-//     if (audioRef.current && "MediaSource" in window) {
-//       const mediaSource = new MediaSource();
-//       audioRef.current.src = URL.createObjectURL(mediaSource);
+  //   useEffect(() => {
+  //     if (audioRef.current && "MediaSource" in window) {
+  //       const mediaSource = new MediaSource();
+  //       audioRef.current.src = URL.createObjectURL(mediaSource);
 
-//       mediaSource.addEventListener("sourceopen", () => {
-//         const sourceBuffer = mediaSource.addSourceBuffer(
-//           'video/mp4; codecs="mp4a.40.2"'
-//         );
-//         const audioUrl = mp3; // URL вашего аудиофайла в формате MP4 с AAC аудио
+  //       mediaSource.addEventListener("sourceopen", () => {
+  //         const sourceBuffer = mediaSource.addSourceBuffer(
+  //           'video/mp4; codecs="mp4a.40.2"'
+  //         );
+  //         const audioUrl = mp3; // URL вашего аудиофайла в формате MP4 с AAC аудио
 
-//         fetch(audioUrl)
-//           .then((response) => response.arrayBuffer())
-//           .then((data) => {
-//             sourceBuffer.appendBuffer(data);
-//             audioRef.current.play();
-//           });
-//       });
-//     } else {
-//       console.error("MediaSource not supported.");
-//     }
-//   }, []);
+  //         fetch(audioUrl)
+  //           .then((response) => response.arrayBuffer())
+  //           .then((data) => {
+  //             sourceBuffer.appendBuffer(data);
+  //             audioRef.current.play();
+  //           });
+  //       });
+  //     } else {
+  //       console.error("MediaSource not supported.");
+  //     }
+  //   }, []);
 
-//   return (
-//     <div className={styles.container}>
-//       <audio ref={audioRef} controls>
-//         Your browser does not support the audio element.
-//       </audio>
-//     </div>
-//   );
-// }
+  //   return (
+  //     <div className={styles.container}>
+  //       <audio ref={audioRef} controls>
+  //         Your browser does not support the audio element.
+  //       </audio>
+  //     </div>
+  //   );
+  return <div>Your browser does not support the audio element.</div>;
+}
 
-// export default Portfolio;
+export default Portfolio2;
