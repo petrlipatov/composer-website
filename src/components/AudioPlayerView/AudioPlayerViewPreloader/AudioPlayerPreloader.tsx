f8326015d02cf4baacf94d610c1096349535be4f
Petr Lipatov <lipatov3@gmail.com>
2024-03-30 21:48:45 +0300

    imports fix 2

--- src/components/AudioPlayerView/AudioPlayerViewPreloader/AudioPlayerPreloader.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./AudioPlayerPreloader.module.css";
-
-const PlayerLoader = () => <div className={styles.loader}></div>;
-
-export default PlayerLoader;
--- src/components/AudioPlayerView/types.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-
-export type AudioPlayerViewProps = {
-  index: number;
-  link: string;
-  isAudioTrackSelected: boolean;
-  setSelectedAudioTrack: Dispatch<SetStateAction<number>>;
-};
