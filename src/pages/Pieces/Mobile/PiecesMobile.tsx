26dbb797818c3a3089a6854a97978deb99b2f395
Petr Lipatov <lipatov3@gmail.com>
2024-03-30 17:48:14 +0300

    img src test #2

--- src/pages/Pieces/Mobile/PiecesMobile.tsx
+++ src/pages/Pieces/Mobile/PiecesMobile.tsx
@@ -3,14 +3,7 @@ import Logo from "../../../components/Logo/Logo";
 import { GENRES_PIECES, PIECES } from "../../../utils/constants";
 import s from "./PiecesMobile.module.css";
 import Tag from "../../../components/Tag/Tag";
-// import { Link } from "react-router-dom";
-// import nameSrc from "../../../assets/images/name.svg";
-// import titleSrc from "../../../assets/images/title.svg";
-// import logoSrc from "../../../assets/images/logo_vertical.png";
-// import Toggler from "./Toggler/Toggler";
-// import AudioTrack from "../../../components/AudioTrack/AudioTrack";
-// import mp3Src from "../../../assets/Theory-of-Light-Master.mp3";
-// import mp3Src2 from "../../../assets/Free_Test_Data_2MB_MP3.mp3";
+import img from "./../../../../public/images/track.webp";
 
 function PiecesMobile() {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
