c68467b3b81087daa732edf1d14b3a33fa434f3c
Petr Lipatov <lipatov3@gmail.com>
2024-04-25 18:39:07 +0300

    .

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -60,57 +60,63 @@ function Info() {
         </div> */}
 
         <div className={s.textSection} ref={textContainerRef}>
-          <div className={s.heading}>composer</div>
-          <ul className={s.list}>
-            <li className={s.listItem}>&gt; Classical music background</li>
-            <li className={s.listItem}>
-              &gt; Composing since ’96 in classical,
-              <br /> experimental and popular genres
-            </li>
-            <li className={s.listItem}>&gt; Composing for video since 2012</li>
-          </ul>
+          <div className={s.wrapper}>
+            <div className={s.heading}>composer</div>
+            <ul className={s.list}>
+              <li className={s.listItem}>&gt; Classical music background</li>
+              <li className={s.listItem}>
+                &gt; Composing since ’96 in classical,
+                <br /> experimental and popular genres
+              </li>
+              <li className={s.listItem}>
+                &gt; Composing for video since 2012
+              </li>
+            </ul>
 
-          <div className={s.heading}>sound engineer</div>
-          <ul className={s.list}>
-            <li className={s.listItem}>
-              &gt; Studied music technology
-              <br />
-              at London College of Music,
-              <br />
-              first class honours graduate
-            </li>
-            <li className={s.listItem}>&gt; Recording and mixing since 2006</li>
-            <li className={s.listItem}>
-              &gt; Worked and collected experience
-              <br /> at music studios in London,
-              <br /> Berlin, New York and Moscow
-            </li>
-            <li className={s.listItem}>
-              &gt; Had a long talk with Michael Brauer
-              <br />
-              at Electric Lady Studios, NYC in 2012
-              <br />
-              who changed my perception of mixing,
-              <br />
-              forever thankful
-            </li>
-          </ul>
+            <div className={s.heading}>sound engineer</div>
+            <ul className={s.list}>
+              <li className={s.listItem}>
+                &gt; Studied music technology
+                <br />
+                at London College of Music,
+                <br />
+                first class honours graduate
+              </li>
+              <li className={s.listItem}>
+                &gt; Recording and mixing since 2006
+              </li>
+              <li className={s.listItem}>
+                &gt; Worked and collected experience
+                <br /> at music studios in London,
+                <br /> Berlin, New York and Moscow
+              </li>
+              <li className={s.listItem}>
+                &gt; Had a long talk with Michael Brauer
+                <br />
+                at Electric Lady Studios, NYC in 2012
+                <br />
+                who changed my perception of mixing,
+                <br />
+                forever thankful
+              </li>
+            </ul>
 
-          <div className={s.heading}>musician</div>
-          <ul className={s.list}>
-            <li className={s.listItem}>
-              &gt; 1996-2004 Fortepiano
-              <br /> Conservatory class
-            </li>
-            <li className={s.listItem}>
-              &gt; 2003-2012 Drums and vocals classes
-            </li>
-            <li className={s.listItem}>&gt; Guitar and bass - self-taught</li>
-            <li className={s.listItem}>&gt; Performed on stage since 2002</li>
-            <li className={s.listItem}>
-              &gt; 2006-2013 Leader of an indie band
-            </li>
-          </ul>
+            <div className={s.heading}>musician</div>
+            <ul className={s.list}>
+              <li className={s.listItem}>
+                &gt; 1996-2004 Fortepiano
+                <br /> Conservatory class
+              </li>
+              <li className={s.listItem}>
+                &gt; 2003-2012 Drums and vocals classes
+              </li>
+              <li className={s.listItem}>&gt; Guitar and bass - self-taught</li>
+              <li className={s.listItem}>&gt; Performed on stage since 2002</li>
+              <li className={s.listItem}>
+                &gt; 2006-2013 Leader of an indie band
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className={s.contactsSection}>
