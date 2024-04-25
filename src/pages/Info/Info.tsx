0966206bb737fdf6a4c1d15e5dfeceac32bb359c
Petr Lipatov <lipatov3@gmail.com>
2024-04-25 19:45:39 +0300

    .

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -35,7 +35,7 @@ function Info() {
         container,
         minSize = 15,
         maxSize = 25,
-        step = 0.3,
+        step = 0.1,
         unit = "px",
       }) => {
         let i = minSize;
