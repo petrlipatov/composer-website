2a913abde48ec12d1fe6f510ec5f15b23b32660c
Petr Lipatov <lipatov3@gmail.com>
2024-04-25 21:44:35 +0300

    .

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -6,14 +6,9 @@ import image from "../../assets/images/portrait.jpg";
 import whatsappSrc from "../../assets/images/whatsapp.svg";
 
 import s from "./Info.module.css";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 function Info() {
-  const [widthData, setwidthData] = useState({
-    windowInnerWidth: 0,
-    screenWidth: 0,
-    availableScreenWidth: 0,
-  });
   const textSectionRef = useRef<HTMLDivElement>();
   const textContainerRef = useRef<HTMLDivElement>();
 
@@ -55,21 +50,12 @@ function Info() {
         container.style.fontSize = `${i - step}${unit}`;
         console.log(container.style.fontSize);
       };
-      // resizeText({
-      //   section: textSectionRef.current,
-      //   container: textContainerRef.current,
-      // });
+      resizeText({
+        section: textSectionRef.current,
+        container: textContainerRef.current,
+      });
     }
   }, [textSectionRef.current, textContainerRef.current]);
-
-  useEffect(() => {
-    if ((window.innerWidth, screen.width)) {
-      const windowInnerWidth = window.innerWidth;
-      const screenWidth = screen.width;
-      const availableScreenWidth = screen.availWidth;
-      setwidthData({ windowInnerWidth, screenWidth, availableScreenWidth });
-    }
-  }, [window.innerWidth, screen.width]);
 
   return (
     <div className={s.page}>
@@ -87,66 +73,63 @@ function Info() {
         </div> */}
 
         <div className={s.textSection} ref={textSectionRef}>
-          {/* <div className={s.wrapper} ref={textContainerRef}> */}
-          <div
-            className={s.heading}
-          >{`widthData.windowInnerWidth ${widthData.windowInnerWidth}`}</div>
-          <div
-            className={s.heading}
-          >{`widthData.screenWidth ${widthData.screenWidth}`}</div>
-          <div
-            className={s.heading}
-          >{`widthData.availableScreenWidth ${widthData.availableScreenWidth}`}</div>
-          <div className={s.heading}>composer</div>
-          <ul className={s.list}>
-            <li className={s.listItem}>&gt; Classical music background</li>
-            <li className={s.listItem}>
-              &gt; Composing since ’96 in classical,
-              <br /> experimental and popular genres
-            </li>
-            <li className={s.listItem}>&gt; Composing for video since 2012</li>
-          </ul>
-          <div className={s.heading}>sound engineer</div>
-          <ul className={s.list}>
-            <li className={s.listItem}>
-              &gt; Studied music technology
-              <br />
-              at London College of Music,
-              <br />
-              first class honours graduate
-            </li>
-            <li className={s.listItem}>&gt; Recording and mixing since 2006</li>
-            <li className={s.listItem}>
-              &gt; Worked and collected experience
-              <br /> at music studios in London,
-              <br /> Berlin, New York and Moscow
-            </li>
-            <li className={s.listItem}>
-              &gt; Had a long talk with Michael Brauer
-              <br />
-              at Electric Lady Studios, NYC in 2012
-              <br />
-              who changed my perception of mixing,
-              <br />
-              forever thankful
-            </li>
-          </ul>
-          <div className={s.heading}>musician</div>
-          <ul className={s.list}>
-            <li className={s.listItem}>
-              &gt; 1996-2004 Fortepiano
-              <br /> Conservatory class
-            </li>
-            <li className={s.listItem}>
-              &gt; 2003-2012 Drums and vocals classes
-            </li>
-            <li className={s.listItem}>&gt; Guitar and bass - self-taught</li>
-            <li className={s.listItem}>&gt; Performed on stage since 2002</li>
-            <li className={s.listItem}>
-              &gt; 2006-2013 Leader of an indie band
-            </li>
-          </ul>
-          {/* </div> */}
+          <div className={s.wrapper} ref={textContainerRef}>
+            <div className={s.heading}>composer</div>
+            <ul className={s.list}>
+              <li className={s.listItem}>&gt; Classical music background</li>
+              <li className={s.listItem}>
+                &gt; Composing since ’96 in classical,
+                <br /> experimental and popular genres
+              </li>
+              <li className={s.listItem}>
+                &gt; Composing for video since 2012
+              </li>
+            </ul>
+
+            <div className={s.heading}>sound engineer</div>
+            <ul className={s.list}>
+              <li className={s.listItem}>
+                &gt; Studied music technology
+                <br />
+                at London College of Music,
+                <br />
+                first class honours graduate
+              </li>
+              <li className={s.listItem}>
+                &gt; Recording and mixing since 2006
+              </li>
+              <li className={s.listItem}>
+                &gt; Worked and collected experience
+                <br /> at music studios in London,
+                <br /> Berlin, New York and Moscow
+              </li>
+              <li className={s.listItem}>
+                &gt; Had a long talk with Michael Brauer
+                <br />
+                at Electric Lady Studios, NYC in 2012
+                <br />
+                who changed my perception of mixing,
+                <br />
+                forever thankful
+              </li>
+            </ul>
+
+            <div className={s.heading}>musician</div>
+            <ul className={s.list}>
+              <li className={s.listItem}>
+                &gt; 1996-2004 Fortepiano
+                <br /> Conservatory class
+              </li>
+              <li className={s.listItem}>
+                &gt; 2003-2012 Drums and vocals classes
+              </li>
+              <li className={s.listItem}>&gt; Guitar and bass - self-taught</li>
+              <li className={s.listItem}>&gt; Performed on stage since 2002</li>
+              <li className={s.listItem}>
+                &gt; 2006-2013 Leader of an indie band
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className={s.contactsSection}>
