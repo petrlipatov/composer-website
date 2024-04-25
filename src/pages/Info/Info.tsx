63a80949bbbc5fc1fcfa28ddec42e7d8eff8949d
Petr Lipatov <lipatov3@gmail.com>
2024-04-25 19:59:59 +0300

    .

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -47,7 +47,7 @@ function Info() {
           if (!overflow) i += step;
         }
 
-        const modifiedStep = step * 3;
+        const modifiedStep = step * 5;
 
         container.style.fontSize = `${i - modifiedStep}${unit}`;
       };
