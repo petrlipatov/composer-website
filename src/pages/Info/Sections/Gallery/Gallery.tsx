131f6bcb562e8d0c8769d5b3579ab2cf19fb396f
Petr Lipatov <lipatov3@gmail.com>
2024-04-27 18:27:10 +0300

    .

--- src/pages/Info/Sections/Gallery/Gallery.tsx
+++ src/pages/Info/Sections/Gallery/Gallery.tsx
@@ -2,16 +2,23 @@ import s from "./Gallery.module.css";
 import { GALLERY_IMAGES } from "../../../../utils/constants/index.ts";
 
 function Gallery() {
+  const pagesCount = GALLERY_IMAGES.length / 6;
+
   return (
     <div className={s.section}>
-      {GALLERY_IMAGES.map((el, i) => {
-        return (
-          <div className={s.container} key={i}>
-            <img className={s.image} src={el.src} />
-            <div className={s.caption}>{el.desc}</div>
-          </div>
-        );
-      })}
+      <div className={s.imagesGrid}>
+        {GALLERY_IMAGES.map((el, i) => {
+          return (
+            <div className={s.container} key={i}>
+              <img className={s.image} src={el.src} />
+              <div className={s.caption}>{el.desc}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={s.pagesCountContainer}>
+        <div className={s.pagesCount}>{`${pagesCount}/${pagesCount}`}</div>
+      </div>
     </div>
   );
 }
