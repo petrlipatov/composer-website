2da044b8fc3bd4ab357b7b264b9065ed3d052d38
Petr Lipatov <lipatov3@gmail.com>
2024-04-30 20:09:19 +0300

    .

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,77 @@
-import MobileHome from "./Mobile/MobileHome";
-import { useContext } from "react";
-import { ScreenSizeContext } from "../../components/Layout/Layout";
+import React, { useRef, RefObject, useState, Suspense } from "react";
+import { gsap } from "gsap";
+import { Observer } from "gsap/Observer";
+import { Link } from "react-router-dom";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-export default function Home() {
-  const screenSize = useContext(ScreenSizeContext);
+import Modal from "../../components/Modal/Modal";
+import Preloader from "../../components/Preloader/Preloader";
+import Logo from "../../components/Logo/Logo";
+
+import flower1_src from "../../assets/images/flower1.webp";
+import flower2_src from "../../assets/images/flower2.webp";
+import flower3_src from "../../assets/images/flower3.webp";
+
+import s from "./Home.module.css";
+
+const YouTubePlayer = React.lazy(
+  () => import("../../components/YoutubePlayer/YoutubePlayer")
+);
+
+gsap.registerPlugin(ScrollTrigger, Observer);
+
+function Home() {
+  const [isPopupOpened, setPopupState] = useState(false);
+
+  const pageRef: RefObject<HTMLDivElement> = useRef(null);
+
+  function openPopup() {
+    setPopupState(true);
+  }
+
   return (
-    <>{screenSize.width < 480 ? <MobileHome /> : <div>Desktop comp</div>}</>
+    <div className={s.page} ref={pageRef}>
+      <div className={s.content} ref={pageRef}>
+        <Logo />
+        <div className={s.nav}>
+          <div className={s.button} onClick={openPopup}>
+            Showreel
+          </div>
+
+          <Link to="work" className={s.link}>
+            Featured Work
+          </Link>
+
+          <Link to="pieces" className={s.link}>
+            Pieces
+          </Link>
+
+          <Link to="info" className={s.link}>
+            Info
+          </Link>
+        </div>
+        <div
+          className={s.image}
+          style={{ backgroundImage: `url(${flower1_src})` }}
+        />{" "}
+        <div
+          className={s.image}
+          style={{ backgroundImage: `url(${flower2_src})` }}
+        />{" "}
+        <div
+          className={s.image}
+          style={{ backgroundImage: `url(${flower3_src})` }}
+        />
+        {isPopupOpened && (
+          <Modal setPopupState={setPopupState}>
+            <Suspense fallback={<Preloader content={"🐥"} />}>
+              <YouTubePlayer videoId="u0dBG0AL3Cs" />
+            </Suspense>
+          </Modal>
+        )}
+      </div>
+    </div>
   );
 }
+
+export default Home;
--- src/pages/Pieces/Pieces.tsx
+++ src/pages/Pieces/Pieces.tsx
@@ -1,18 +1,152 @@
-import PiecesMobile from "./Mobile/PiecesMobile";
-// import { useContext } from "react";
-// import { ScreenSizeContext } from "../../components/Layout/Layout";
+import { Suspense, useState, useRef, useMemo } from "react";
+import { Link } from "react-router-dom";
 
-// import Logo from "../../components/Logo/Logo";
+import AudioPlayer from "../../components/AudioPlayer/AudioPlayer";
+import AudioTrack from "../../components/AudioTrack/AudioTrack";
+import Logo from "../../components/Logo/Logo";
+import Tag from "../../components/Tag/Tag";
+import Modal from "../../components/Modal/Modal";
+import Preloader from "../../components/Preloader/Preloader";
+import YouTubePlayer from "../../components/YoutubePlayer/YoutubePlayer";
+
+import { AudioTrackData } from "../../types";
+
+import { PIECES, PIECES_GENRES } from "../../utils/constants";
+import closeIconSrc from "../../assets/images/close-icon.svg";
+
+import s from "./Pieces.module.css";
+
+export type PlayingAudioData = {
+  index: number;
+  name: string;
+  imageSource: string;
+  videoSource: string;
+};
 
 function Pieces() {
-  // const screenSize = useContext(ScreenSizeContext);
-  return <PiecesMobile />;
+  const [videoId, setVideoId] = useState<string>("");
+  const [isVideoPopupOpened, setVideoPopupState] = useState(false);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [selectedTrack, setSelectedTrack] = useState<number>(null);
+
+  const [isPlayerOpened, setIsPlayerOpened] = useState(false);
+  const [playingAudioData, setPlayingAudioData] = useState<
+    PlayingAudioData | undefined
+  >();
+
+  const audioPlayerRef = useRef<HTMLAudioElement>();
+
+  const filteredPieces = useMemo(
+    () => filterPiecesByTags(selectedTags, PIECES),
+    [selectedTags]
+  );
+
+  const handleTagClick = (tag: string) => {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(selectedTags.filter((t) => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  function handleClearTagsClick() {
+    setSelectedTrack(null);
+    setSelectedTags([]);
+  }
+
+  const isTagSelected = (tag: string) => {
+    return selectedTags.includes(tag);
+  };
+
+  const isTagDisabled = (tag: string) => {
+    return !filteredPieces.some((piece) => piece.tags.includes(tag));
+  };
+
+  function filterPiecesByTags(
+    selectedTags: string[],
+    pieces: AudioTrackData[]
+  ) {
+    return pieces.filter((piece) => {
+      return selectedTags.every((tag) => piece.tags.includes(tag));
+    });
+  }
+
+  function openVideoPopup() {
+    setVideoPopupState(true);
+  }
+
+  return (
+    <div className={s.page}>
+      <div className={s.content}>
+        <div className={s.nav}>
+          <Link to="/" className={s.pageTitle}>
+            Pieces
+          </Link>
+          <Logo />
+        </div>
+
+        <div className={s.tagsSection}>
+          <div className={s.tagsList}>
+            {PIECES_GENRES.map((genre, i) => (
+              <Tag
+                name={genre}
+                isSelected={isTagSelected(genre)}
+                isDisabled={isTagDisabled(genre)}
+                onClick={() => handleTagClick(genre)}
+                key={i}
+              />
+            ))}
+            <div className={s.tagsButton} onClick={handleClearTagsClick}>
+              <img className={s.closeIcon} src={closeIconSrc} />
+              No filter
+            </div>
+          </div>
+        </div>
+
+        <audio preload="none" ref={audioPlayerRef}>
+          <source src={""} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+
+        <div className={s.tracksSection}>
+          {filteredPieces.map((track, index) => (
+            <AudioTrack
+              index={index}
+              data={track}
+              setPlayingAudioData={setPlayingAudioData}
+              setVideoId={setVideoId}
+              openPopup={openVideoPopup}
+              setIsPlayerOpened={setIsPlayerOpened}
+              isSelected={selectedTrack === index}
+              setSelectedTrack={setSelectedTrack}
+              ref={audioPlayerRef}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+
+      <AudioPlayer
+        isPlayerOpened={isPlayerOpened}
+        setIsPlayerOpened={setIsPlayerOpened}
+        playingAudioData={playingAudioData}
+        setPlayingAudioData={setPlayingAudioData}
+        setSelectedTrack={setSelectedTrack}
+        setVideoId={setVideoId}
+        openPopup={openVideoPopup}
+        filteredPieces={filteredPieces}
+        ref={audioPlayerRef}
+      />
+
+      {isVideoPopupOpened && (
+        <Modal setPopupState={setVideoPopupState}>
+          <Suspense fallback={<Preloader content={"🐥"} />}>
+            <YouTubePlayer videoId={videoId} />
+          </Suspense>
+        </Modal>
+      )}
+    </div>
+  );
 }
 
 export default Pieces;
-
-// return (
-//   <>
-//     {screenSize.width < 480 ? <PortfolioMobile /> : <div>Desktop comp</div>}
-//   </>
-// );
