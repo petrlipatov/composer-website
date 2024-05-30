345838b7d42fa316bd8706df3a1890df1aa2a717
Petr Lipatov <lipatov3@gmail.com>
2024-05-30 20:08:58 +0300

    .

--- src/components/AudioPlayer/Extended/Info/Info.tsx
+++ src/components/AudioPlayer/Extended/Info/Info.tsx
@@ -1,4 +1,4 @@
-import { ProjectData } from "../../../../types";
+import { ProjectData } from "../../../../pages/FeaturedWork/_types";
 
 import videoIcon from "../../../../assets/images/tv.svg";
 
--- src/components/AudioPlayer/Extended/Title/Title.tsx
+++ src/components/AudioPlayer/Extended/Title/Title.tsx
@@ -1,4 +1,4 @@
-import { ProjectData } from "../../../../types";
+import { ProjectData } from "../../../../pages/FeaturedWork/_types";
 import s from "./Title.module.css";
 
 type Props = {
